4108257e6e6c3f065b1aff9068cca96d422e46a3
根子 裕行 <hneko@hneko-03.local>
2023-06-27 13:59:48 +0900

    feat: 🎸 hallengeProblems-easy-1,383

---
+++ solutions/typescript/easy/1.two-sum.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+
+    const result: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                result[0] = i;
+                result[1] = j;
+            }
+        }
+    }
+    return result;
+};
+
+// @lc code=end
